52df1c33c1a8282aa3230f32883ee4a49e36072a
Rokas <rookas.rudzenskas@gmail.com>
2022-09-20 15:45:36 +0300

    Rokas

--- utils/const.js
+++ utils/const.js
@@ -7,4 +7,6 @@ export const STABLE_POOL_PROGRAM_ID = new PublicKey(
     '5wSMdEYxSW7iB3rdE7c8yB3bqBUrVtvggjDdY5viyDLk',
 )
 
-export const STABLE_POOL_IDL = spotify
+export const STABLE_POOL_IDL = spotify;
+
+// https://muddy-shy-dawn.solana-devnet.quiknode.pro/5b638e723dd3df716748cb5432458a2e21e53e05/
